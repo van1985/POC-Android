8d77fc072451ffee8afb3d1c0b99a3b6690e4c86
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-10-20 16:58:27 -0300

    Add login material design

--- app/src/main/java/com/poc_android/activities/MainActivity.java
+++ app/src/main/java/com/poc_android/activities/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getActionBar().hide();
         setContentView(R.layout.activity_main);
 
 
